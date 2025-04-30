7b896d663b9d29933286b75e794b1379c59829c3
pascalb44 <pascalboucault@aol.com>
2025-04-30 15:52:45 +0200

    cors

--- ouest-camions-api/config/cors.php
+++ ouest-camions-api/config/cors.php
@@ -6,7 +6,9 @@ return [
     
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+//    'allowed_origins' => ['http://localhost:3000'],
+
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
