fe006760117f948ec44b4238bf1280b4b1222287
pascalb44 <pascalboucault@aol.com>
2025-05-02 12:25:44 +0200

    push test

--- ouest-camions-react/src/pages/admin/categories-trailers/CategoriesTrailers.jsx
+++ ouest-camions-react/src/pages/admin/categories-trailers/CategoriesTrailers.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom';
 
-
 const CategoriesTrailers = () => {
     const [categoriesTrailers, setCategoriesTrailers] = useState([]);
 
