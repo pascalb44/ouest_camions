5de91dd926b86f5709bbce7f43496d0a87ed5e2c
pascalb44 <pascalboucault@aol.com>
2025-05-05 14:07:48 +0200

    test cors

--- ouest-camions-api/config/cors.php
+++ ouest-camions-api/config/cors.php
@@ -5,12 +5,11 @@ return [
     'paths' => ['api/*', 'admin/*', 'sanctum/csrf-cookie'],
     
     'allowed_methods' => ['*'],
-
-//   'allowed_origins' => ['http://localhost:3000'], //local = ok
-// 'allowed_origins' => ['http://168.231.80.181'],   //site =  non
-
-// 'allowed_origins' => ['http://srv807237.hstgr.cloud:8000'], //site
-
+    'allowed_origins' => [
+        'http://localhost:3000',            // Local (fonctionne pour ton développement)
+        'http://168.231.80.181',            // IP de ton site
+        'http://srv807237.hstgr.cloud:8000' // Site en ligne
+    ],
 
 
     'allowed_origins' => ['*'],
