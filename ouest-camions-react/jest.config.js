0307ddac3723068705bc6828a4ea7efca91fbb9d
pascalb44 <pascalboucault@aol.com>
2025-08-27 18:38:05 +0200

    jest config for tests 27081838

--- ouest-camions-react/jest.config.js
+++ ouest-camions-react/jest.config.js
@@ -3,20 +3,21 @@ const path = require('path');
 module.exports = {
     // Charge env variables before each test
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // setup testing-library après env
-    // Transforme with Babel
+    // Transform with Babel
     transform: {
         '^.+\\.[jt]sx?$': 'babel-jest',
     },
 
     transformIgnorePatterns: [
-        'node_modules/(?!(axios|date-fns/.*))'],
+        'node_modules/(?!(axios|date-fns)/.*)'
+    ],
 
 
     // Mock style files (css/scss)
     moduleNameMapper: {
         '\\.(css|scss|sass)$': path.resolve(__dirname, 'styleMock.js'),
         '\\.(gif|ttf|eot|svg|png|mp4)$': path.resolve(__dirname, 'fileMock.js'),
-        '^axios$': require.resolve('axios/dist/node/axios.cjs')
+        '^axios$': require.resolve('axios')
     },
 
     testEnvironment: 'jsdom',
--- ouest-camions-react/jest.setup.js
+++ ouest-camions-react/jest.setup.js
@@ -1,2 +1,10 @@
 require('dotenv').config({ path: '.env.testing' });
 require('@testing-library/jest-dom');
+
+// jest.setup.js
+require('dotenv').config({ path: '.env.testing' });
+require('@testing-library/jest-dom');
+
+// Mocks to avoid bugs in node_modules
+jest.mock('axios');
+jest.mock('date-fns/locale', () => ({ fr: {} }));
