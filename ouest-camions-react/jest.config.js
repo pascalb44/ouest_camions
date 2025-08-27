5be7014172a539f277858cedb5562efae7f82016
pascalb44 <pascalboucault@aol.com>
2025-08-27 16:10:53 +0200

    test wamp27081610

--- ouest-camions-react/jest.config.js
+++ ouest-camions-react/jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
 
     transformIgnorePatterns: [
-        '/node_modules/(?!(axios|date-fns)/)',
+        '\\\\node_modules\\\\(?!(axios|date-fns\\\\.*))',
     ],
 
     // Mock les fichiers styles (css/scss)
