17bd29a884f38db8b49073f673d4f4ff367ec5b1
pascalb44 <pascalboucault@aol.com>
2025-08-27 17:43:35 +0200

    jest config 27081743

--- ouest-camions-react/jest.config.js
+++ ouest-camions-react/jest.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    // Charge les variables d'environnement avant chaque test
+    // Charge env variables before each test
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // setup testing-library après env
-    // Transformer avec Babel (tu dois avoir un babel.config.js ou .babelrc configuré)
+    // Transforme with Babel
     transform: {
         '^.+\\.[jt]sx?$': 'babel-jest',
     },
 
     transformIgnorePatterns: [
-        "node_modules/(?!axios|date-fns)"
+        "node_modules/(?!axios/.*|date-fns/.*)"
     ],
 
 
-    // Mock les fichiers styles (css/scss)
+    // Mock style files (css/scss)
     moduleNameMapper: {
         '\\.(css|scss|sass)$': path.resolve(__dirname, 'styleMock.js'),
         '\\.(gif|ttf|eot|svg|png|mp4)$': path.resolve(__dirname, 'fileMock.js'),
+        '^axios$': require.resolve('axios/dist/node/axios.cjs')
     },
 
     testEnvironment: 'jsdom',
