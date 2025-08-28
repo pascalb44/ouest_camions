0f8b3cc5515569d6fd8555e02e60d7ceec93c586
pascalb44 <pascalboucault@aol.com>
2025-08-28 10:01:09 +0200

    unit test 280810

--- ouest-camions-react/src/unit/HomeSubtitle.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders La location de qualité link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('La location de qualité');
-  expect(linkElement).toBeInTheDocument();
-});
- 
